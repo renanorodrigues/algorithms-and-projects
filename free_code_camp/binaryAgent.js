d061e2c67417215d37a4cb2bafd232ee8c0a0666
Renan Oliveira <renan.oliveira.webdesigner@gmail.com>
2021-04-13 16:12:42 -0300

    Algorithm to convert binary in text.

---
+++ free_code_camp/binaryAgent.js
@@ -0,0 +1,15 @@
+/*
+# The Problem
+
+Return an English translated sentence of the passed binary string.
+The binary string will be space separated.
+*/
+
+function binaryAgent(codeBinary){
+  let string = codeBinary.concat(); 
+
+  // The method fromCharCode needs the Unicode like argument
+  return string.split(" ").map(binary => String.fromCharCode(parseInt(binary, 2))).join("");
+}
+
+console.log(binaryAgent("01000001 01110010 01100101 01101110 00100111 01110100 00100000 01100010 01101111 01101110 01100110 01101001 01110010 01100101 01110011 00100000 01100110 01110101 01101110 00100001 00111111"))

92d8ac8affa96859f430c1d95fb488573ce0c930
Renan Oliveira <renan.oliveira.webdesigner@gmail.com>
2021-04-28 07:22:05 -0300

    Recursive version of binary search

--- algorithms/binary_search.js
+++ algorithms/binary_search.js
@@ -23,4 +23,22 @@ function binary_search(arr, response = null){
   return null;
 }
 
+function recursive_binary_search(arr, value, lower=0, high=null){
+  arr.sort((a, b) => a - b);
+  
+  if(high < lower) return null;
+  if(high === null) high = arr.length - 1;
+
+  let middle = Math.floor((high + lower) / 2);
+
+  if(arr[middle] === value){
+    return middle;
+  }else if(arr[middle] > value){
+    return recursive_binary_search(arr, value, lower, middle - 1);
+  }else{
+    return recursive_binary_search(arr, value, middle + 1, high);
+  }
+}
+
 binary_search([2, 3, 6, 1, 0], 3)
+recursive_binary_search([2, 3, 6, 1, 0], 3)

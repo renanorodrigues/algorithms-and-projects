0a607d9aaceaefe89ad189d5d457491f70de4e2f
Renan Oliveira <renan.oliveira.webdesigner@gmail.com>
2021-05-13 07:22:16 -0300

    [u] bubble_sort, observation in loop

--- exercicies/bubble_sort_string.js
+++ exercicies/bubble_sort_string.js
@@ -2,6 +2,8 @@ const bubble_sort = (arr) => {
   let chars = [...arr];
   const length = chars.length;
 
+  // Nesse algoritmo, não é preciso que o index seja o último elemento
+  // Pois a comparação é feita do penúltimo com o último
   for (let index = 0; index < length - 1; index++) {
     for (let i = 0; i < length - 1; i++) {
       if(chars[i].charCodeAt() > chars[i + 1].charCodeAt()){
